9b4c80fb82b7a2ad149a8d4000f979bdd785b67c
Utsab <arnob.4124@g.bracu.ac.bd>
2023-02-08 16:43:55 +0600

    Here I have worked on increment and decrement of product cart

--- src/components/GenericCard/index.tsx
+++ src/components/GenericCard/index.tsx
@@ -1,45 +1,45 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { useState } from 'react';
 import { products } from '../../data';
 import './index.scss';
-import { Button, Typography } from '@mui/material';
+import { Typography } from '@mui/material';
 import AddCircleRoundedIcon from '@mui/icons-material/AddCircleRounded';
 import RemoveCircleRoundedIcon from '@mui/icons-material/RemoveCircleRounded';
 
 
+
 const GenericCard = () => {
-    const [counter, setCounter] = useState(0);
-    const incrementHandler = () => {
-        setCounter((val) => val + 1)
+    const [counter, setCounter] = useState<Record<string, number>>({});
+    const incrementHandler = (index: number) => {
+        setCounter({...counter, [index]: (counter[index] ? counter[index] : 0) + 1})
     }
-    const decrementHandler = () => {
-        setCounter((val) => val - 1)
+    const decrementHandler = (index: number) => {
+        setCounter({...counter, [index]: (counter[index] ? counter[index] : 1) - 1})
+        
+
     }
 
+    console.log(counter)
 
     return (
         <main>
             {
                 products.map((product, index) => (
-
-                    <div className='GenericCard__Screen'>
-                        <div className='GenericCard__ProductDetails'>
-                        <Typography className='GenericCard__Title' variant="body1" key={index} >{product.title}</Typography>
-                        <Typography className='GenericCard__Description' variant="body2" key={index} >{product.description}</Typography>
-                    </div>    
-                        <img className="GenericCard__Image" src={product.thumb} alt="g"/>
-                        <Typography className="GenericCard__Price" variant="body2" >{product.price}</Typography>
-                        <div className="GenericCard__button">
-                            <RemoveCircleRoundedIcon className="GenericCard__Icon" onClick={decrementHandler} />
-                            <Typography className="grid-item">{counter}</Typography>
-                            <AddCircleRoundedIcon className="GenericCard__Icon" onClick={incrementHandler} />
-                            
+                    <Fragment key={index} >
+                        <div className='GenericCard__Screen'>
+                            <div className='GenericCard__ProductDetails'>
+                                <Typography className='GenericCard__Title' variant="body1" >{product.title}</Typography>
+                                <Typography className='GenericCard__Description' variant="body2" >{product.description}</Typography>
+                            </div>
+                            <img className="GenericCard__Image" src={product.thumb} alt="g" />
+                            <Typography className="GenericCard__Price" variant="body2" >{product.price}</Typography>
+                            <div className="GenericCard__button">
+                                <RemoveCircleRoundedIcon className="GenericCard__Icon" onClick={() => { decrementHandler(index) }} />
+                                <Typography className="grid-item">{counter[index] ? counter[index] : 0}</Typography>
+                                <AddCircleRoundedIcon className="GenericCard__Icon" onClick={() => { incrementHandler(index) }} />
+                            </div>
                         </div>
-
-                    </div>
-
-
-
+                    </Fragment>
                 ))
             }
         </main>
@@ -48,3 +48,4 @@ const GenericCard = () => {
 }
 
 export default GenericCard;
+
