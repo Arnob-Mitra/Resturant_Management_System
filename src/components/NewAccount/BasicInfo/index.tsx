d9bb9ef595f33ee8ce8faf0c68cbf1fa18e95b86
Utsab <arnob.4124@g.bracu.ac.bd>
2023-02-20 12:23:19 +0600

    Basic Info

--- src/components/NewAccount/BasicInfo/index.tsx
+++ src/components/NewAccount/BasicInfo/index.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import './index.scss';
 import {Button, TextField, Typography,} from "@mui/material";
-import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
+import {DatePicker } from '@mui/x-date-pickers/DatePicker';
 import dayjs, { Dayjs } from 'dayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+
 
 const BasicInfo = () => {
   const [value, setValue] = React.useState<Dayjs | null>(dayjs('2022-04-07'));
@@ -12,13 +15,25 @@ const BasicInfo = () => {
       <TextField variant='outlined' label='Full Name' required type="name" className='BasicInfo_TextField' />
       <TextField variant='outlined' label='Email Address' required type='email' className='BasicInfo_TextField' />
       
-
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DatePicker
+          className='BasicInfo_TextField'
+          label="Date of Birth"
+          openTo="day"
+          views={['year', 'month', 'day']}
+          value={value}
+          onChange={(newValue: React.SetStateAction<dayjs.Dayjs | null>) => {
+            setValue(newValue);
+          }}
+          renderInput={(params) => <TextField   {...params} />}
+        />
+           </LocalizationProvider>
       <div className="BasicInfo__Gender">
       </div>
-
-      <Button variant='contained' id="BasicInfo__button" >Skip</Button>
-      <Button variant='contained' id="SignUpPage__button" >Continue</Button>
-
+      <div id="BasicInfo__button">
+        <Button variant='contained' className="BasicInfo__Skipbutton" >Skip </Button>
+        <Button variant='contained' className="BasicInfo__Continuebutton" >Continue </Button>
+      </div>
 
     </div>
   )
